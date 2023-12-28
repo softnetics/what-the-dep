34d1dce7d1ce3915ea25710c444e395e0e1784e6
msp5382 <meen.sankaew@gmail.com>
2023-12-28 10:10:58 +0700

    feat: multiple export path

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/plugin.ts", "src/container.ts"],
+  entry: ["src/index.ts", "src/plugin.ts", "src/container.ts"],
   splitting: true,
   minify: true,
   clean: true,
